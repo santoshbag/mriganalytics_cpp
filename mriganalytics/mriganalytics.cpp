00cb0b90b00a2e04502fbf59812e6b5b8554af52
santoshbag <santosh.bag@gmail.com>
2020-05-11 12:19:03 +0530

    MRIG CPP

---
+++ mriganalytics/mriganalytics.cpp
@@ -0,0 +1,14 @@
+﻿// mriganalytics.cpp : Defines the entry point for the application.
+//
+
+#include "mriganalytics.h"
+
+
+using namespace std;
+
+int main()
+{
+	cout << "Starting Mriganalytcis CPP.";
+
+	return 1;
+}
---
+++ mriganalytics/mriganalytics.h
@@ -0,0 +1,10 @@
+﻿// mriganalytics.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+#include <pqxx/pqxx> 
+
+// TODO: Reference additional headers your program requires here.
